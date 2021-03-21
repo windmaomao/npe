bdd5f2ee0ec1fad6300023000c3c5c401228d380
Fang Jin <windmaomao@gmail.com>
2021-03-21 16:16:49 -0400

    add basic build for index html and js

---
+++ packages/utils/index.js
@@ -0,0 +1,11 @@
+function resolve() {
+  console.log('resolve 6')
+}
+
+resolve()
+
+import confetti from 'canvas-confetti'
+confetti.create(document.getElementById('canvas'), {
+  resize: true,
+  useWorker: true
+})({ particleCount: 200, spread: 200 })
