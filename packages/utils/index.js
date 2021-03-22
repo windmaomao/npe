f41fb4abc13cecc30772fca6bd891705016e759b
Fang Jin <windmaomao@gmail.com>
2021-03-21 23:02:41 -0400

    add lit element

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -9,3 +9,13 @@ confetti.create(document.getElementById('canvas'), {
   resize: true,
   useWorker: true
 })({ particleCount: 200, spread: 200 })
+
+import { LitElement, html } from 'lit-element'
+
+class NpeHeader extends LitElement {
+  render() {
+    return html`<p>Header</p>`
+  }
+}
+
+window.customElements.define('npe-header', NpeHeader)
